eed724cbcfd222b0180fdcaba8c042840fca06f4
Pannetier <nael.pannetier@outlook.fr>
2020-10-16 14:30:45 +0200

    add button colors and new functions attack
    the attack engage for the character an offensive of the monsters

--- game.php
+++ game.php
@@ -11,26 +11,28 @@
     <body class="container-fluid">
         <h1 class="text-center"> Game </h1>
         <div class="row" id ="principalRow">
-            <div class="col" id="persoCol">
-                <div id="imagePerso"  class="row d-flex justify-content-center">
-                </div>
-                <div class="row d-flex justify-content-center">
-                    <p id="namePerso"></p>
-                </div>
-                <div class="row d-flex justify-content-center">
-                    <p id="hpPerso"></p>
-                </div>
-                <div class="row d-flex justify-content-center">
-                    <button class="persoAttack btn btn-success btn_game" onclick="attack('Attack', 'Magie', 'Soin')"> Attaquer </button>
-                </div>
-                <div class="row d-flex justify-content-center mt-3">
-                    <button class="persoMagie btn btn-success btn_game" onclick="attack('Magie', 'Attack', 'Soin')"> Magie </button>
-                </div>
-                <div class="row d-flex justify-content-center mt-3">
-                    <button class="persoSoin btn btn-success btn_game" onclick=""> Soigner </button>
-                    <div class="cancelBtn">     
-                        <button class="cancel btn btn-success btn_game" onclick="cancel('Soin', 'Magie', 'Attack')"> Annuler </button>
-                    </div>    
+            <div class="col d-flex justify-content-center" id="persoCol">
+                <div id="border">
+                    <div id="imagePerso"  class="row d-flex justify-content-center">
+                    </div>
+                    <div class="row d-flex justify-content-center">
+                        <p id="namePerso"></p>
+                    </div>
+                    <div class="row d-flex justify-content-center">
+                        <p id="hpPerso"></p>
+                    </div>
+                    <div id="CancelButton" class="row d-flex justify-content-center">
+                        <button class="cancel btn btn-danger btn_game" onclick="cancel('Attack', 'Magie', 'Soin')"> Annuler </button>
+                    </div>
+                    <div id="AttackCancel" class="row d-flex justify-content-center">
+                        <button class="persoAttack btn btn-primary btn_game" onclick="attack('Attack', 'Magie', 'Soin')"> Attaquer </button>
+                    </div>
+                    <div id="MagieCancel" class="row d-flex justify-content-center mt-3">
+                        <button class="persoMagie btn btn-primary btn_game" onclick="attack('Magie', 'Attack', 'Soin')"> Magie </button>
+                    </div>
+                    <div id="SoinCancel" class="row d-flex justify-content-center mt-3">
+                        <button class="persoSoin btn btn-primary btn_game" onclick=""> Soigner </button>   
+                    </div>
                 </div>
             </div>
             <!-- Création dynamique des monstres -->
--- js/perso.js
+++ js/perso.js
@@ -1,3 +1,6 @@
+// Randomize the number of monsters that appear
+var numbMonsters = Math.ceil(Math.random() * 3);
+
 class persos {
 
 
@@ -13,20 +16,29 @@ class persos {
 
         this.hpMax;
 
-        this.image;
-
     }
 
-    attackMonsters(numMonsters) {
+    persoAttackMonsters(numMonsters) {
         this.hp = this.hp - perso.attack;
         if(this.hp <= 0) {
-            $("#monsters" + numMonsters).html('Vous avez vaincu ' + this.name) + ' !';
+            $("#monsters" + numMonsters).html('Vous avez vaincu : ' + this.name) + ' !';
             setTimeout(function() {
-                $("#monstersCol" + numMonsters).html('');
-                console.log('monstersCol' + numMonsters);
+                $("#monstersCol" + numMonsters).css('visibility', 'hidden');
+                cancel('Attack', 'Magie', 'Soin');
             }, 3000);
         } else {
             $("#monsters" + numMonsters).html(this.hp + '/' + this.hpMax);
+            cancel('Attack', 'Magie', 'Soin');
+            this.monstersAttackPerso();
+        }
+    }
+
+    monstersAttackPerso() {
+        for (i = 0; i < numbMonsters; i++) {
+            setTimeout(function () {
+                perso.hp = perso.hp - aMonsters['attack'][i];
+                $("#hpPerso").html("<strong>" + perso.hp + "/" + perso.hpMax + " HP</strong>");
+            }, 1500);
         }
     }
 
@@ -49,20 +61,22 @@ perso.attack = 12;
 monsters1.name = 'Titan (8 mètres)'
 monsters1.hp = 15;
 monsters1.hpMax = 15;
-
+monsters1.attack = 5;
 
 monsters2.name = 'Titan (10 mètres)'
 monsters2.hpMax = 17;
 monsters2.hp = 17;
+monsters2.attack = 7;
 
 monsters3.name = 'Titan (13 mètres)'
 monsters3.hp = 18;
 monsters3.hpMax = 18;
+monsters3.attack = 12;
 
 monsters4.name = 'Titan Feminin'
 monsters4.hp = 19;
 monsters4.hpMax = 19;
-
+monsters4.attack = 20;
 
 
 var aMonsters = [];
@@ -84,41 +98,46 @@ aMonsters['name'] = [
     monsters4.name
 ]
 
+aMonsters['attack'] = [
+    monsters1.attack,
+    monsters2.attack,
+    monsters3.attack,
+    monsters4.attack
+]
 
-
-console.log(aMonsters['hp']);
-
-// Randomize the number of monsters that appear
-var numbMonsters = Math.ceil(Math.random() * 3);
 $(document).ready(function () {
     for (i = 0; i < numbMonsters; i++) {
         var concat = 'monsters' + i;
         console.log(concat);
-        $("#monsters").append(`<div class='row mb-5'><div class= 'col' id='monstersCol` + i + `' ><div class='row d-flex justify-content-center'><p> Image Monstre </p></div><div class='row d-flex justify-content-center'><p> ` + aMonsters['name'][i] + ` </p></div><div class='row d-flex justify-content-center'><p id='monsters`+ i +`'> `+ aMonsters['hp'][i] +` </p></div><div class='row d-flex justify-content-center'><button class='btnMonsterAttack btn btn-success mt-3 btn_game' onclick='monsters` + (i + 1) + `.attackMonsters(` + i + `)'> Attaquer </button></div></div ></div >`);
+        $("#monsters").append(`<div class='row mb-5'><div class= 'col monstersColDimension' id='monstersCol` + i + `' ><div class='row d-flex justify-content-center'><p> Image Monstre </p></div><div class='row d-flex justify-content-center'><p> ` + aMonsters['name'][i] + ` </p></div><div class='row d-flex justify-content-center'><p id='monsters`+ i +`'> `+ aMonsters['hp'][i] +` </p></div><div class='row d-flex justify-content-center'><button class='btnMonsterAttack btn btn-warning mt-3 btn_game' onclick='monsters` + (i + 1) + `.persoAttackMonsters(` + i + `)'> Attaquer </button></div></div ></div >`);
     }
     $(".btnMonsterAttack").css("display", "none");
 });
 
 
 
-$("#namePerso").html(perso.name);
-$("#hpPerso").html(perso.hp + "/" + perso.hpMax);
+$("#namePerso").html("<strong>" + perso.name + "</strong>");
+$("#hpPerso").html("<strong>" + perso.hp + "/" + perso.hpMax + " HP</strong>");
 $("#imagePerso").html(`<img class="imgPerso" src='img/` + perso.name + `.png' />`);
 
 
 
 function attack(actionAttack, none1Attack, none2Attack) {
     $(".btnMonsterAttack").css("display", "block");
-    $(".perso" + actionAttack).css("display", "none");
-    $(".perso" + none1Attack).css("display", "none");
-    $(".perso" + none2Attack).css("display", "none");
-    $(".cancelBtn").css("display", "block");
+    $("#" + actionAttack + "Cancel").css("visibility", "hidden");
+    $("#" + none1Attack + "Cancel").css("visibility", "hidden");
+    $("#" + none2Attack + "Cancel").css("visibility", "hidden");
+    $("#CancelButton").css("visibility", "visible");
+    $("#CancelButton").css("height", "0px");
+    $("#CancelButton").css("margin-top", "0px");
 }
 
 function cancel(action, none1, none2) {
     $(".btnMonsterAttack").css("display", "none");
-    $(".perso" + action).css("display", "block");
-    $(".perso" + none1).css("display", "block");
-    $(".perso" + none2).css("display", "block");
-    $(".cancelBtn").css("display", "none");
+    $("#" + action + "Cancel").css("visibility", "visible");
+    $("#" + none1 + "Cancel").css("visibility", "visible");
+    $("#" + none2 + "Cancel").css("visibility", "visible");
+    $("#CancelButton").css("visibility", "hidden");
+    $("#CancelButton").css("height", "0px");
+    $("#CancelButton").css("margin-top", "0px");
 }
