5ae635d9632c1510c4c97b3756221126d6f1df28
Pannetier <nael.pannetier@outlook.fr>
2020-10-22 14:19:57 +0200

    continues animations
    New function heal + animations on monsters attack

--- js/perso.js
+++ js/perso.js
@@ -1,11 +1,12 @@
 // Randomize the number of monsters that appear
-var numbMonsters = Math.ceil(Math.random() * 4);
+var numbMonsters = Math.ceil(Math.random() * 3);
 var VtempaHp = [];
 var aAttack = [];
 aAttack = [2, 1.1, 1.1, 0.9, 0.9];
 var attackPerso;
 var vTempHp;
 var vTempHpMax;
+var vTempNameMonsters;
 
 class persos {
 
@@ -32,15 +33,19 @@ class persos {
         attackPerso = (Math.floor(perso.attack * aAttack[(Math.floor(Math.random() * 5))]))
         this.hp = this.hp - attackPerso;
         if(this.hp <= 0) {
-            $("#monsters" + numMonsters).html('Vous avez vaincu : ' + this.name) + ' !';
+            this.animateDamage(numMonsters, attackPerso);
+            vTempNameMonsters = this.name;
+            setTimeout(function () {
+                $("#monsters" + numMonsters).html('Vous avez vaincu : ' + vTempNameMonsters) + ' !';
+            }, 1000);
             setTimeout(function() {
                 $("#monstersCol" + numMonsters).css('visibility', 'hidden');
                 cancel('Attack', 'Magie', 'Soin');
-            }, 1000);
+            }, 2000);
             cancel('Attack', 'Magie', 'Soin');
             perso.xp = perso.xp + this.xp;
             this.levelUp();
-            $("#xpPerso").html("<strong>" + perso.xp + "/" + xpMax + "</strong>");
+            $("#xpPerso").html("<strong>" + perso.xp + "/" + xpMax + " XP</strong>");
             numbMonsters = numbMonsters - 1;
             // Check if at least 1 monsters is still alive
             if (numbMonsters > 0) {
@@ -52,13 +57,7 @@ class persos {
 
             }
         } else {
-            $("#monsters" + numMonsters).html("- " + attackPerso + "HP !");
-            console.log(this.hp);
-            vTempHp = this.hp;
-            vTempHpMax = this.hpMax;
-            setTimeout(function () {
-                $("#monsters" + numMonsters).html(vTempHp + '/' + vTempHpMax);
-            }, 1000);
+            this.animateDamage(numMonsters, attackPerso)
             cancel('Attack', 'Magie', 'Soin');
             monstersAttackPerso();
         }
@@ -72,13 +71,28 @@ class persos {
                 perso.xp = perso.xp - xpMax;
                 xpMax = perso.level * 5;
             }
-            $("#levelPerso").html("<strong>" + perso.level + "</strong>");
+            $("#levelPerso").html("<strong>Level " + perso.level + "</strong>");
             $("#levelUp").html(perso.level);
             $("#showLevel").css("animation", "bright 3s");
             setTimeout(function () {
                 $("#showLevel").css("animation", "");
             }, 3000);
         }
+    }
+
+    animateDamage(numMonsters, attackPerso) {
+        $("#monsters" + numMonsters).html(" - " + attackPerso + "HP !</strong>");
+        $("#monsters" + numMonsters).css("color", "red");
+        $("#monsters" + numMonsters).css("font-weight", "800");
+        $("#monsters" + numMonsters).addClass("animationAttackedText");
+        vTempHp = this.hp;
+        vTempHpMax = this.hpMax;
+        setTimeout(function () {
+            $("#monsters" + numMonsters).html(vTempHp + '/' + vTempHpMax);
+            $("#monsters" + numMonsters).css("color", "black");
+            $("#monsters" + numMonsters).css("font-weight", "400");
+            $("#monsters" + numMonsters).removeClass("animationAttackedText");
+        }, 1000);
     }
 
     
@@ -107,6 +121,7 @@ monsters1.hpMax = 15;
 monsters1.attack = 5;
 monsters1.level = 7;
 monsters1.xp = 70;
+monsters1.image = 'reiner2';
 
 monsters2.name = 'Titan (10 mètres)'
 monsters2.hpMax = 17;
@@ -114,6 +129,7 @@ monsters2.hp = 17;
 monsters2.attack = 10;
 monsters2.level = 10;
 monsters2.xp = 80;
+monsters2.image = 'reiner2'; ;
 
 monsters3.name = 'Titan (13 mètres)'
 monsters3.hp = 18;
@@ -121,6 +137,7 @@ monsters3.hpMax = 18;
 monsters3.attack = 15;
 monsters3.level = 14;
 monsters3.xp = 160;
+monsters3.image = 'reiner2'; ;
 
 monsters4.name = 'Titan Feminin'
 monsters4.hp = 19;
@@ -128,6 +145,7 @@ monsters4.hpMax = 19;
 monsters4.attack = 20;
 monsters4.level = 25;
 monsters4.xp = 270;
+monsters4.image = 'reiner2'; ;
 
 
 var aMonsters = [];
@@ -156,9 +174,35 @@ aMonsters['attack'] = [
     monsters4.attack
 ]
 
+aMonsters['image'] = [
+    monsters1.image,
+    monsters2.image,
+    monsters3.image,
+    monsters4.image
+]
+
 $(document).ready(function () {
     for (i = 0; i < numbMonsters; i++) {
-        $("#monsters").append(`<div class='row mb-5'><div class= 'col monstersColDimension' id='monstersCol` + i + `' ><div class='row d-flex justify-content-center'><p> Image Monstre </p></div><div class='row d-flex justify-content-center'><p> ` + aMonsters['name'][i] + ` </p></div><div class='row d-flex justify-content-center'><p id='monsters`+ i +`'> `+ aMonsters['hp'][i] +` </p><p id=degat` + i + `> </p></div><div class='row d-flex justify-content-center'><button class='btnMonsterAttack btn btn-warning mt-3 btn_game' onclick='monsters` + (i + 1) + `.persoAttackMonsters(` + i + `)'> Attaquer </button></div></div ></div >`);
+        $("#monsters").append(`
+        <div class='monstersRow row mb-5'>
+            <div class= 'col monstersColDimension d-flex justify-content-center' id='monstersCol` + i + `' >
+                <div id="borderMonsters" class="">
+                    <div class='row d-flex justify-content-center'>
+                        <img class='imgMonsters' src='img/` + aMonsters['image'][i] + `.png' </p>
+                    </div>
+                    <div class='row d-flex justify-content-center'>
+                        <p> ` + aMonsters['name'][i] + ` </p>
+                    </div>
+                    <div class='row d-flex justify-content-center'>
+                        <p id='monsters`+ i +`'> `+ aMonsters['hp'][i] +` </p>
+                        <p id=degat` + i + `> </p>
+                    </div>
+                    <div class='row d-flex justify-content-center'>
+                        <button class='btnMonsterAttack btn btn-warning mt-3 btn_game' onclick='monsters` + (i + 1) + `.persoAttackMonsters(` + i + `)'> Attaquer </button>
+                    </div>
+                </div >
+            </div>
+        </div>`);
     }
     $(".btnMonsterAttack").css("display", "none");
 });
@@ -168,8 +212,8 @@ var xpMax = perso.level * 5;
 $("#namePerso").html("<strong>" + perso.name + "</strong>");
 $("#hpPerso").html("<strong>" + perso.hp + "/" + perso.hpMax + " HP</strong>");
 $("#imagePerso").html(`<img class="imgPerso" src='img/` + perso.name + `.png' />`);
-$("#levelPerso").html("<strong>" + perso.level + "</strong>");
-$("#xpPerso").html("<strong>" + perso.xp + "/" + xpMax + "</strong>");
+$("#levelPerso").html("<strong>Level " + perso.level + "</strong>");
+$("#xpPerso").html("<strong>" + perso.xp + "/" + xpMax + " XP</strong>");
 
 
 
